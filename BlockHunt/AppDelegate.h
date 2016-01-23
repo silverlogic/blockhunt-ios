55bc904f53a878f5a1e45acdc6477df33313eb08
David Hartmann <dh@tsl.io>
2016-01-23 11:51:19 -0500

    Initial Commit

---
+++ BlockHunt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlockHunt
+//
+//  Created by David Hartmann on 1/23/16.
+//  Copyright © 2016 SilverLogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlockHunt/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  BlockHunt
+//
+//  Created by David Hartmann on 1/23/16.
+//  Copyright © 2016 SilverLogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
