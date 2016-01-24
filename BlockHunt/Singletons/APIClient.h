00239a7530fb2e2eba2f4a3a083f81de54a6420d
David Hartmann <dh@tsl.io>
2016-01-24 02:28:39 -0500

    added missing methods to APIClient header

--- BlockHunt/Singletons/APIClient.h
+++ BlockHunt/Singletons/APIClient.h
@@ -15,6 +15,8 @@
 + (instancetype)sharedClient;
 
 + (BOOL)isAuthenticated;
++ (NSString*)getToken;
++ (void)setToken:(NSString*)token;
 
 + (void)loginWithFacebookToken:(NSString*)accessToken success:(void (^)(User *user))success failure:(void (^)(NSError *error, NSHTTPURLResponse *response))failure;
 + (void)getStoresAroundLocation:(CLLocationCoordinate2D)coordinate success:(void (^)(NSArray *stores))success failure:(void (^)(NSError *error, NSHTTPURLResponse *response))failure;
