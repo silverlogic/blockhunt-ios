0ef457c3c3917413ab0d9800e8be15650edcee41
Cristina <ce@tsl.io>
2016-01-24 04:18:36 -0500

    non-cookie cutter send view

---
+++ BlockHunt/ViewControllers/SendViewController.h
@@ -0,0 +1,13 @@
+//
+//  SendViewController.h
+//  BlockHunt
+//
+//  Created by Cristina on 1/24/16.
+//  Copyright © 2016 SilverLogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SendViewController : UIViewController
+
+@end
